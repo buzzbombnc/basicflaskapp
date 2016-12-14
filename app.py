b2b4c17db7671855339daf450bde12fc4a19970b
buzzbombnc <buzzbomb-github@buzzbomb.net>
2016-12-13 19:17:04 -0500

    Basic 1.0 app with hostname output.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import socket
+
 from flask import Flask
 app = Flask(__name__)
 
+VERSION="1.0"
+
 @app.route("/")
 def root():
-    return "root"
+    return "%s    %s" % (VERSION, socket.gethostname())
 
 if __name__ == "__main__":
     app.run()
