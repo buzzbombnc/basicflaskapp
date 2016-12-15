934fcc27f694aa324490b224d899add232ce6273
buzzbombnc <buzzbomb-github@buzzbomb.net>
2016-12-15 13:04:13 -0500

    Add some fake work to app.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import socket
+import random
+import time
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,7 +9,9 @@ VERSION="1.0"
 
 @app.route("/")
 def root():
-    return "%s    %s" % (VERSION, socket.gethostname())
+    # Do "work" for up to one second.
+    time.sleep(random.random())
+    return "%s    %s\n" % (VERSION, socket.gethostname())
 
 if __name__ == "__main__":
     app.run()
