ef60dd618fc3806ed51367acddd40fe49aa0447a
buzzbombnc <buzzbomb-github@buzzbomb.net>
2016-12-17 01:44:16 -0500

    Update app output and tests for same.

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 import socket
-import random
-import time
 
 from flask import Flask
 app = Flask(__name__)
 
-VERSION="1.0"
-
 @app.route("/")
 def root():
-    # Do "work" for up to one second.
-    time.sleep(random.random())
-    return "%s    %s\n" % (VERSION, socket.gethostname())
+    return "Greetings from server %s\n" % socket.gethostname()
 
 if __name__ == "__main__":
     app.run()
